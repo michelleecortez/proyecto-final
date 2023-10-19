368f1533043bf4008ecc62058e6dd995e2f1843b
dantsss-dev <danieliseopc@gmail.com>
2023-10-18 23:01:08 -0600

    :art: change route

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -3,8 +3,7 @@ import MenuItem from "./MenuItem";
 const Menu = () => {
   return (
     <div className="col-2 bg-primary ">
-      <MenuItem path="/" label="Inicio de pagina" />
-      <MenuItem path="/Ejemplo" label="Inicio de pagina" />
+      <MenuItem path="/" label="Home" />
       <MenuItem path="/cellphones" label="Celulares" />
       <MenuItem path="/videogames" label="Juegos" />
     </div>
